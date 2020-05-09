3c062e5b488d51d55e46c1af854d569f118a81b0
hakantekgul <tekgul2@illinois.edu>
2020-05-09 15:23:12 +0300

    all

--- src/app.py
+++ src/app.py
@@ -33,6 +33,27 @@ def load_image_bytes(raw_bytes: ByteString) -> Image:
     img = open_image(BytesIO(raw_bytes))
     return img
 
+class ConvolutionalNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        # (((224−2)/2)−2)/2=54.5 (2 layers of pooling)
+        self.conv1 = nn.Conv2d(3, 6, 3, 1)
+        self.conv2 = nn.Conv2d(6, 16, 3, 1)
+        self.fc1 = nn.Linear(54*54*16, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 3)
+
+    def forward(self, X):
+        X = F.relu(self.conv1(X))
+        X = F.max_pool2d(X, 2, 2)
+        X = F.relu(self.conv2(X))
+        X = F.max_pool2d(X, 2, 2)
+        X = X.view(-1, 54*54*16)
+        X = F.relu(self.fc1(X))
+        X = F.relu(self.fc2(X))
+        X = self.fc3(X)
+        return F.log_softmax(X, dim=1)
+
 
 def predict(img, n: int = 3) -> Dict[str, Union[str, List]]:
     pred_class, pred_idx, outputs = model.predict(img)
